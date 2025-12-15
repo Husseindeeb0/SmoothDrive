916b2f718d842422cee213e9772a270a9ad0db10
Hussein <deebh776@gmail.com>
2025-12-15 21:55:16 +0200

    Move landing to root & fix footer path problem

--- js/components.js
+++ js/components.js
@@ -1,8 +1,13 @@
+const scriptTag = document.currentScript;
+const scriptSrc = scriptTag.src;
+const projectRoot = scriptSrc.substring(0, scriptSrc.lastIndexOf("/js/"));
+const footerPath = `${projectRoot}/components/footer.html`;
+
 document.addEventListener("DOMContentLoaded", function () {
   // Load Footer
   const footerPlaceholder = document.getElementById("footer-placeholder");
   if (footerPlaceholder) {
-    fetch("/components/footer.html")
+    fetch(footerPath)
       .then((response) => {
         if (!response.ok) {
           throw new Error(`Failed to load footer: ${response.statusText}`);
